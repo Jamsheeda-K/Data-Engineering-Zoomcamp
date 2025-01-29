f1be80e4228e266ed2181a0cf8046d2969ea105f
Jamsheeda-K <jamsheedak@gmail.com>
2025-01-29 15:01:20 +0100

    terraform demo

---
+++ 01-docker-terraform/terraform demo/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.18.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = var.region
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "arctic-welder-449311-e8-bucket"
+  location      = var.location
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+resource "google_bigquery_dataset" "demo-dataset" {
+  dataset_id = "example_dataset"
+  location   = var.location
+}
